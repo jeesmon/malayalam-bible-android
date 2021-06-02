552fb593ed0bdcf66e2263559bab009de233aec3
Godly T.Alias <godlytalias@yahoo.co.in>
2021-06-03 01:18:26 +0530

    Changing db path to avoid write privilege issues
    Signed-off-by: Godly T.Alias <godlytalias@yahoo.co.in>

--- MalayalamBible/src/main/java/com/jeesmon/malayalambible/providers/MalayalamBibleBookmarksContentProvider.java
+++ MalayalamBible/src/main/java/com/jeesmon/malayalambible/providers/MalayalamBibleBookmarksContentProvider.java
@@ -189,7 +189,7 @@ public class MalayalamBibleBookmarksContentProvider extends ContentProvider {
 			if (Environment.MEDIA_MOUNTED.equals(state)) {
 				mDbPath = Environment.getExternalStorageDirectory()
 						.getAbsolutePath()
-						+ "/Android/data/com.jeesmon.malayalambible/db";
+						+ "/Android/com.jeesmon.malayalambible/db";
 			} else {
 				mDbPath = INTERNAL_DB_PATH;
 			}
